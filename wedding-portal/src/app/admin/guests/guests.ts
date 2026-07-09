0508ff66eb91012e9a3e1d5fff6f270982cba75c
--help <sathishc0298@gmail.com>
2026-07-09 21:53:16 +0530

    project setup

--- wedding-portal/src/app/admin/guests/guests.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-guests',
-  imports: [],
-  templateUrl: './guests.html',
-  styleUrl: './guests.scss',
-})
-export class Guests {}
--- wedding-portal/src/app/features/story/story.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-story',
-  imports: [],
-  templateUrl: './story.html',
-  styleUrl: './story.scss',
-})
-export class Story {}
