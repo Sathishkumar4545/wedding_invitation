458f225f1867c7334decf67fbd92f28b1962602b
--help <sathishc0298@gmail.com>
2026-07-13 19:25:26 +0530

    Added New Features

---
+++ wedding-invitation/src/app/admin/guests/guests.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guests',
+  imports: [],
+  templateUrl: './guests.html',
+  styleUrl: './guests.css',
+})
+export class Guests {}
